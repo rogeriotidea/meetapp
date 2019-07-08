352f32efd96de5f85d57a177d14916e0e461b6ce
Rogerio <dev@tidea.com.br>
2019-07-07 21:02:59 -0300

    inicio

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'a34e544bc882619b3464ab413f8ac491',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: '192.168.99.100',
+  username: 'root',
+  password: '123456',
+  database: 'meetapp',
+  port: '33061',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
